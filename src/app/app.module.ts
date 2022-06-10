cd84dcd6d582f917cbc71a61c78074a1d06010a6
Fituning <carstar@live.fr>
2022-06-10 15:44:24 +0200

    Revert "import fixed"
    This reverts commit 7504ef6e34053bb84b90aa3a3746764d4e7c0efe.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire/compat'
-import { AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import { AngulareFirestoreModule} from '@angular/fire/compat/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -41,10 +41,10 @@ import { ProfilComponent } from './profil/profil.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    AppRoutingModule
   ],
   providers: [
-    ArticleService,
+    ArticleService
   ],
   bootstrap: [AppComponent]
 })
