7504ef6e34053bb84b90aa3a3746764d4e7c0efe
Fituning <carstar@live.fr>
2022-06-10 14:59:02 +0200

    import fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'utxshot',
+    appId: '1:292599312486:web:7dfa7738419644fb5ffda7',
+    databaseURL: 'https://utxshot-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'utxshot.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCxUGJFPpx0u3UoI6sJj44wrYDDcQK3jMA',
+    authDomain: 'utxshot.firebaseapp.com',
+    messagingSenderId: '292599312486',
+    measurementId: 'G-EFK2V838L9',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,15 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCxUGJFPpx0u3UoI6sJj44wrYDDcQK3jMA",
-    authDomain: "utxshot.firebaseapp.com",
-    projectId: "utxshot",
-    storageBucket: "utxshot.appspot.com",
-    messagingSenderId: "292599312486",
-    appId: "1:292599312486:web:7dfa7738419644fb5ffda7",
-    measurementId: "G-EFK2V838L9"
+    projectId: 'utxshot',
+    appId: '1:292599312486:web:7dfa7738419644fb5ffda7',
+    databaseURL: 'https://utxshot-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'utxshot.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCxUGJFPpx0u3UoI6sJj44wrYDDcQK3jMA',
+    authDomain: 'utxshot.firebaseapp.com',
+    messagingSenderId: '292599312486',
+    measurementId: 'G-EFK2V838L9',
   }
 };
 
